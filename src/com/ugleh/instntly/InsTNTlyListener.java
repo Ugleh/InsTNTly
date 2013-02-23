341bed8d812ee51ca70d5b89baa222cc74d9391a
Ugleh <troti@ymail.com>
2013-02-23 16:48:08 -0500

    Updated to support multiple TNT connected 1 redstone wire.

--- src/com/ugleh/instntly/InsTNTlyListener.java
+++ src/com/ugleh/instntly/InsTNTlyListener.java
@@ -1,5 +1,7 @@
 package com.ugleh.instntly;
 
+import java.util.ArrayList;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -19,24 +21,26 @@ public class InsTNTlyListener implements Listener{
 	public void BlockRedstone(BlockRedstoneEvent event){
 		if(event.getNewCurrent() >= 1){ //If the current of the block, be it a lever or redstone wire is ON
 			Block blk = event.getBlock(); //Set the events block to the variable 'blk', because we are going to be using this alot.
-			Block tntBlk = null; //This will be the TNT Block when we find it.
-			Boolean tntExists = false;
+			ArrayList<Block> tntBlocks = new ArrayList<Block>(); //There may be more than 1 TNT block connected to a redstone wire.
+			Boolean tntExists = false; //I would of checked if tntBlk was null, but that creates a NPE, so I made a Boolean variable.
 			if(blk.getRelative(BlockFace.NORTH).getType().equals(Material.TNT)){
-				tntBlk = blk.getRelative(BlockFace.NORTH);
+				tntBlocks.add(blk.getRelative(BlockFace.NORTH));
 				tntExists = true;
 			}else if(blk.getRelative(BlockFace.EAST).getType().equals(Material.TNT)){
-				tntBlk = blk.getRelative(BlockFace.EAST);
+				tntBlocks.add(blk.getRelative(BlockFace.EAST));
 				tntExists = true;
 			}else if(blk.getRelative(BlockFace.SOUTH).getType().equals(Material.TNT)){
-				tntBlk = blk.getRelative(BlockFace.SOUTH);
+				tntBlocks.add(blk.getRelative(BlockFace.SOUTH));
 				tntExists = true;
 			}else if(blk.getRelative(BlockFace.WEST).getType().equals(Material.TNT)){
-				tntBlk = blk.getRelative(BlockFace.WEST);
+				tntBlocks.add(blk.getRelative(BlockFace.WEST));
 				tntExists = true;
 			}
 			if(tntExists){ //If tntExists is set to true, then there is a TNT Block in the area.
-				tntBlk.setType(Material.AIR); //Sets the tntBlk to air so it does not trigger 2 explosions
-				tntBlk.getWorld().createExplosion(tntBlk.getLocation(), 5); //Create an explosion, same area, same radius.
+				for(Block tntBlk : tntBlocks){
+					tntBlk.setType(Material.AIR); //Sets the tntBlk to air so it does not trigger 2 explosions
+					tntBlk.getWorld().createExplosion(tntBlk.getLocation(), 5); //Create an explosion, same area, same radius.	
+				}
 			}
 			}
 			
